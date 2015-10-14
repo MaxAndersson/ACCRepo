cb1be48ba81937fbf4324fb6175dcc2d646240c1
Erik Li <erkali_limpan@hotmail.com>
2015-10-14 14:16:55 +0200

    tasks start + plan

--- CPOSPTasks.py
+++ CPOSPTasks.py
@@ -5,3 +5,22 @@ import sys
 
 #Celery app object
 celeryapp = Celery('tasks', backend='amqp', broker='amqp://')
+
+
+#Running celery worker server:
+# celery -A tasks.celeryapp worker --loglevel=info
+
+@celeryapp.task(bind = True, name = "cellprofiler")   						
+def cellprofiler_work(self):
+	x=1
+
+
+	return x
+
+
+# Hämta data  (från public object storage)
+# Separera datan..?
+# Köra cellprofiler
+# Forsla ut data igen!   VIlket format... csv eller mysql?
+# Till databas eller concata csv filer
+# Till public object storage?
