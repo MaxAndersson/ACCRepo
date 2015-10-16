e9097d80b3ee9857287f6d61a362bd83a5c7023f
Erik Li <erkali_limpan@hotmail.com>
2015-10-16 15:35:17 +0200

    added celeryconfig file

--- cposp_tasks.py
+++ cposp_tasks.py
@@ -7,6 +7,25 @@ import swiftclient.client
 #Celery app object
 celeryapp = Celery('tasks', backend='amqp', broker='amqp://')
 
+'''
+testing
+CELERY_QUEUES = (
+    Queue('default', Exchange('default'), routing_key='default'),
+    Queue('for_task_A', Exchange('for_task_A'), routing_key='for_task_A'),
+    Queue('for_task_B', Exchange('for_task_B'), routing_key='for_task_B'),
+)
+
+CELERY_ROUTES = {
+    'my_taskA': {'queue': 'for_task_A', 'routing_key': 'for_task_A'},
+    'my_taskB': {'queue': 'for_task_B', 'routing_key': 'for_task_B'},
+}
+'''
+
+
+#http://docs.celeryproject.org/en/latest/reference/celery.html#celery.group
+# Creates a group of tasks to be executed in parallel.
+
+
 
 
 
